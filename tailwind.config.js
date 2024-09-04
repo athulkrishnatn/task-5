030f228c95af859b4c1a94dddd2ba5b8dbc804c1
axxl2121 <athulkrishnagvr2121@gmail.com>
2024-09-04 23:59:40 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        width: {
+          '124': '31rem', // or use the value you prefer
+        },
+      },
+    },
+    plugins: [],
+  }
+  
